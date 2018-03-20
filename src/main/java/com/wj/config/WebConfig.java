af9872d6ca3214f6f7f0a32d91668d2a21dfc43a
wangjun <wangjun@11max.com>
2018-03-20 14:19:01 +0800

    lyl网站

--- src/main/java/com/wj/config/WebConfig.java
+++ src/main/java/com/wj/config/WebConfig.java
@@ -1,13 +1,10 @@
 package com.wj.config;
 
-import com.wj.interceptor.HttpLogInterceptor;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.ResourceUtils;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -20,19 +17,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 
 @Configuration
-@EnableWebMvc
-@ComponentScan
 public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
 
     private ApplicationContext applicationContext;
 
-    /*@Override
+    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX+"/static/");
         registry.addResourceHandler("/templates/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX+"/templates/");
-
         super.addResourceHandlers(registry);
-    }*/
+    }
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
